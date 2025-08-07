c15eb9dfdc19b3bf6717ba50266b766f3f3ffd6e
WelterZaychik <1821855131@qq.com>
2025-08-07 20:34:43 +0800

    项目构建

---
+++ src/main/java/asia/welter/PanWeApplication.java
@@ -0,0 +1,11 @@
+package asia.welter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanWeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PanWeApplication.class, args);
+    }
+}
